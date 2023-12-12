bb0cc5231f666325da38212a16ca7fb8f68d4d1c
ZenoMK <zeno.kujawa@gmail.com>
2023-12-12 15:12:39 +0000

    add the data cleaning script

---
+++ processing/hvacData.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import numpy as np
+
+def fahrenheit_to_celsius(x):
+    return (x - 32) * (5/9)
+
+
+hp_hws_temp = pd.read_csv("../data/hp_hws_temp.csv")
+hp_hws_temp["date"] = pd.to_datetime(hp_hws_temp["date"])
+missing = pd.date_range(hp_hws_temp.date.min(), hp_hws_temp.date.max(), freq='1min').difference(hp_hws_temp.date)
+hp_hws_temp.resample("1T").fillna(method=None)
+#hp_new = pd.date_range(hp_hws_temp, start = "2018-01-01 00:00:00", end = "2020-01-01 00:00:00", freq="15min")#hp_hws_temp.iloc[::15]
+#hp_new.loc[:,"hp_hws_temp"] = fahrenheit_to_celsius(hp_new["hp_hws_temp"])
+print(missing)
---
+++ processing/readme.md
@@ -0,0 +1,6 @@
+### Documentation of data processing
+
+## `hp_hws_temp.csv`
+
+	
+The variable `hp_hws_temp` is only available until May 2nd, 2020. Propose to cut out 2020. 
